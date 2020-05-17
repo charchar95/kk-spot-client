e77d49fbf032ef962c6388e9a0d295dc08e6a03d
Charlie Humphrey <charliehumphrey@Charlies-MacBook-Pro.local>
2020-05-17 12:08:32 -0700

    holy grail layout success

--- src/App.js
+++ src/App.js
@@ -23,12 +23,20 @@ class App extends React.Component {
 
   
     render () {
-  return (
-    <>
-
-    </>
-  );
-}
+    return (
+      <div className="container">
+          <header> 
+            <h1>KK Spot</h1>
+            <img src="https://i.imgur.com/U1zxNlO.png" width="50px" height="50px"></img>
+            </header>
+            <nav className="nav">
+              <h1>This is the nav</h1>
+            </nav>
+            <main className="content"> here is the content</main>
+        <footer className="nowplaying"> this is the footer</footer>
+      </div>
+       );
+    }
   }
 
 export default App;
