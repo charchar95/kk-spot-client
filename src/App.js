cce1d20b34ac87874a58ee17126d4af6dd4a6888
Charlie Humphrey <charliehumphrey@Charlies-MacBook-Pro.local>
2020-05-17 13:08:20 -0700

    grid styling for album photos

--- src/App.js
+++ src/App.js
@@ -27,12 +27,24 @@ class App extends React.Component {
       <div className="container">
           <header> 
             <h1>KK Spot</h1>
-            <img src="https://i.imgur.com/U1zxNlO.png" width="50px" height="50px"></img>
+            <img src="https://i.imgur.com/U1zxNlO.png" width="50px" height="50px" alt="kk slider"></img>
             </header>
             <nav className="nav">
               <h1>This is the nav</h1>
             </nav>
-            <main className="content"> here is the content</main>
+            <main className="content">
+                {this.state.songs.map(song => {
+                  return (
+                  <div className="box" key={song.id}>
+                    
+                      <img src={song.album} width="100px" height="100px" alt={song.name}></img>
+                      <p>{song.name}</p>
+                      
+                    </div>
+                    )
+            })}
+               
+               </main>
         <footer className="nowplaying"> this is the footer</footer>
       </div>
        );
