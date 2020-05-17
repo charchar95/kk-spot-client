e6c9fe96744783c9b4ed21d7a1f7ffacc16049a3
Charlie Humphrey <charliehumphrey@Charlies-MacBook-Pro.local>
2020-05-17 11:09:49 -0700

    get fetch request working

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
-function App() {
+
+
+class App extends React.Component {
+  state = {
+    songs: []
+  }
+  componentDidMount(){
+    this.getSongs()
+    
+  }
+
+  getSongs () {
+    fetch('http://localhost:3000/songs')
+    .then(response => response.json())
+    .then(json => this.setState({ songs: json }))
+    .catch(error => console.error(error))
+  }
+
+
+  
+    render () {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+
+    </>
   );
 }
+  }
 
 export default App;
