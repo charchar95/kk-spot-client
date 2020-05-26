98b5c7d1c16251185704612d27e015334b17800d
Charlie Humphrey <charliehumphrey@Charlies-MacBook-Pro.local>
2020-05-26 13:42:23 -0700

    updated urls for heroku

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ handleDropDown = (arr)=> {
 
 
   getSongs () {
-    fetch('https://kk-spot-api.herokuapp.com/songs')
+    fetch('https://kk-spotify-api.herokuapp.com/songs')
     .then(response => response.json())
     .then(json => this.setState({ songs: json }))
     .catch(error => console.error(error))
